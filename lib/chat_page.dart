ecd371132a134bd7152833ec4caa0fea3aa82a76
Aimbor <aimbor.info@gmail.com>
2024-08-28 23:45:26 +0530

    awertghvb

---
+++ lib/chat_page.dart
@@ -0,0 +1,101 @@
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _openAI = OpenAI.instance.build(
+      token: 'OPENAI_API_KEY',
+      baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 1)),
+      enableLog: true,
+  );
+  final ChatUser _user = ChatUser(
+    id: '1',
+    firstName: 'Charles',
+    lastName: 'Leclerc',
+  );
+
+  final ChatUser _gptChatUser = ChatUser(
+    id: '2',
+    firstName: 'Chat',
+    lastName: 'GPT',
+  );
+
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final List<ChatUser> _typingUsers = <ChatUser>[];
+
+  @override
+  void initState() {
+    super.initState();
+    /*_messages.add(
+      ChatMessage(
+        text: 'Hey!',
+        user: _user,
+        createdAt: DateTime.now(),
+      ),
+    );*/
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(0, 166, 126, 1),
+        title: const Text('Flutter Chat-GPT', style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.w600),
+        ),
+      ),
+      body: DashChat(
+        currentUser: _user,
+        messageOptions: const MessageOptions(
+          currentUserContainerColor: Colors.black,
+          containerColor: Color.fromRGBO(0, 166, 126, 1),
+          textColor: Colors.white,
+        ),
+        onSend: (ChatMessage m) {
+          getChatResponse(m);
+        },
+        messages: _messages,
+        typingUsers: _typingUsers,
+      ),
+    );
+  }
+
+  Future<void> getChatResponse(ChatMessage m) async {
+    setState(() {
+      _messages.insert(0, m);
+      _typingUsers.add(_gptChatUser);
+    });
+    List<Map<String, dynamic>> messagesHistory = _messages.reversed.toList().map((m) {
+      if (m.user == _user) {
+        return Messages(role: Role.user, content: m.text).toJson();
+      } else {
+        return Messages(role: Role.assistant, content: m.text).toJson();
+      }
+    }).toList();
+    final request = ChatCompleteText(
+      messages: messagesHistory,
+      maxToken: 200,
+      model: Gpt4oMiniChatModel(), // gpt-4-0314 old: GptTurbo0301ChatModel
+    );
+    final response = await _openAI.onChatCompletion(request: request);
+    for (var element in response!.choices) {
+      if (element.message != null) {
+        setState(() {
+          _messages.insert(0,
+              ChatMessage(user: _gptChatUser, createdAt: DateTime.now(), text: element.message!.content));
+        });
+      }
+    }
+    setState(() {
+      _typingUsers.remove(_gptChatUser);
+    });
+  }
+}
